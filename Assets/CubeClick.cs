76d1a7e010d1fcdd6675bd1a0e7f96e635e1cc34
Bert Boehmler <aboehmler@chathamfinancial.com>
2016-11-05 16:41:40 -0400

    Spheres coming towards you!

--- Assets/CubeClick.cs
+++ Assets/CubeClick.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+//using GvrController;
 
 public class Germ {
 
@@ -17,13 +20,24 @@ public class Germ {
 
 public class CubeClick : MonoBehaviour {
 
-	public Vector3 startPosition = new Vector3(0, 0, 25);
+	public Vector3 startPosition = new Vector3(0, 0, 75);
 
 	public GameObject Germ1;
 	private float startTimer;
 	public float delayTimer;
 
 	public List<Germ> germs;
+
+	public float speed;
+	public float tilt;
+//	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	Vector3 ZeroPosition = new Vector3(0, 0, 0);
+
+	private System.Random random = new System.Random();
 
 	// Use this for initialization
 	void Start () {
@@ -33,13 +47,32 @@ public class CubeClick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.A)) {
-			Destroy (Germ1);
+
+//		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+//			nextFire = Time.time + fireRate;
+			//Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+
+//		}
+
+		//if (Input.GetKeyDown (KeyCode.A)) {
+
+		//if (Cardboard.SDK.CardboardTriggered) {
+
+/*
+ * 		if (GvrController.ClickButton) {
+			foreach (var germ in germs) {
+				Destroy (germ.GameObject);
+				germ.GameObject = null;
+			}
+			germs.Clear ();
 		}
+	*/
+
 
 		startTimer += Time.deltaTime;
 		if (startTimer >= delayTimer) {
 			startTimer = 0;
+			var startPosition = GetRandomStartPosition ();
 			GameObject tempItem = (GameObject)Instantiate (Germ1, startPosition, transform.rotation);
 			var newGerm = new Germ (tempItem);
 			germs.Add (newGerm);
@@ -52,7 +85,21 @@ public class CubeClick : MonoBehaviour {
     {
 		//transform.position += Vector3.forward * Time.deltaTime * 5;
 		foreach (var germ in germs) {
-			germ.GameObject.transform.position += Vector3.forward * Time.deltaTime * 5;
+			//germ.GameObject.transform.position -= Vector3.forward * Time.deltaTime * 5;
+			germ.GameObject.transform.position = Vector3.Lerp(germ.GameObject.transform.position, transform.position, Time.deltaTime * 1);
+			if (germ.GameObject.transform.position == ZeroPosition) {
+				Destroy (germ.GameObject);
+				germ.GameObject = null;
+			}
 		}
+
+		germs = germs.Where(g => g.GameObject != null).ToList();
     }
+
+	Vector3 GetRandomStartPosition() {
+		var x = (float)random.Next (50, 100);
+		var y = (float)random.Next (50, 100);
+		var z = (float)random.Next (50, 100);
+		return new Vector3 (x, y, z);
+	}
 }
